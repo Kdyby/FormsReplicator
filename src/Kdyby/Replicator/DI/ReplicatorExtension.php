978cf342b782dae6b65eec75a3eab9e22b22a212
Filip Procházka <hosiplan@gmail.com>
2013-03-06 12:58:30 +0100

    ReplicatorExtension

---
+++ src/Kdyby/Replicator/DI/ReplicatorExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Replicator\DI;
+
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class ReplicatorExtension extends Nette\Config\CompilerExtension
+{
+
+	public function afterCompile(Nette\PhpGenerator\ClassType $class)
+	{
+		parent::afterCompile($class);
+
+		$init = $class->methods['initialize'];
+		$init->addBody('Kdyby\Replicator\Container::register();');
+	}
+
+
+
+	/**
+	 * @param \Nette\Config\Configurator $configurator
+	 */
+	public static function register(Nette\Config\Configurator $configurator)
+	{
+		$configurator->onCompile[] = function ($config, Nette\Config\Compiler $compiler) {
+			$compiler->addExtension('formsReplicator', new ReplicatorExtension());
+		};
+	}
+
+}
